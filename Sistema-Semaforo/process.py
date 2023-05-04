cf53ce4b9a24c3aaa1bdc2dc57b6a1a8863452c2
MatheusPatriota <mpatriota55@gmail.com>
2023-05-04 20:48:59 -0300

    feat: adding coments to code

--- Sistema-Semaforo/process.py
+++ Sistema-Semaforo/process.py
@@ -1,26 +1,40 @@
+# Importa as bibliotecas necessárias
 import socket
 import threading
 from random import randint
 from message import Message
 
+# Função para enviar uma solicitação (request) ao servidor
 def send_request():
+    # Cria um socket UDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    process_id = randint(1, 1000)  # Gerando um ID de processo aleatório entre 1 e 1000
+    # Gera um ID de processo aleatório entre 1 e 1000
+    process_id = randint(1, 1000)
+    # Cria uma mensagem do tipo REQUEST com o ID do processo
     message = Message.create_message(Message.REQUEST, process_id)
+    # Envia a mensagem para o servidor no endereço 'localhost' e porta 8888
     sock.sendto(message.encode(), ('localhost', 8888))
+    # Fecha o socket
     sock.close()
 
+# Função para simular várias conexões de processos
 def simulate_multiple_connections(num_processes):
+    # Lista para armazenar as threads
     threads = []
 
+    # Cria e inicia 'num_processes' threads que enviam solicitações ao servidor
     for _ in range(num_processes):
         thread = threading.Thread(target=send_request)
         threads.append(thread)
         thread.start()
 
+    # Aguarda todas as threads serem finalizadas
     for thread in threads:
         thread.join()
 
+# Ponto de entrada do script
 if __name__ == "__main__":
-    num_processes = 5  # número de processos a serem simulados
+    # Define o número de processos a serem simulados
+    num_processes = 5
+    # Chama a função para simular múltiplas conexões
     simulate_multiple_connections(num_processes)
--- Sistema-Semaforo/server.py
+++ Sistema-Semaforo/server.py
@@ -1,13 +1,18 @@
+# Importa as bibliotecas necessárias
 import socket
 import threading
 import datetime
 from message import Message
 import time
 
+# Função para registrar eventos no log.txt
+
 
 def log_event(event):
     with open("log.txt", "a") as log_file:
         log_file.write(event + "\n")
+
+# Classe Server
 
 
 class Server:
@@ -22,62 +27,79 @@ class Server:
         self.socket.bind(('localhost', 8888))
         self.isGranted = False
 
+    # Inicia o servidor
     def start(self):
+        # Registra no log que o servidor está iniciando
         log_event(
             f"[STARTING] SERVIDOR SENDO INICIADO  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+        # Cria a thread para receber mensagens
         self.thread_receive = threading.Thread(target=self.receive)
         self.thread_receive.start()
 
+        # Cria a thread para a interface do usuário
         self.thread_interface = threading.Thread(target=self.interface)
         self.thread_interface.start()
 
+    # Função que recebe mensagens dos processos
     def receive(self):
         while self.running:
             try:
-              data, addr = self.socket.recvfrom(1024)
+                # Recebe os dados e o endereço do processo
+                data, addr = self.socket.recvfrom(1024)
 
-              message_type, process_id, _ = data.decode().split(Message.SEPARATOR)
-              process_id = int(process_id)
+                # Extrai informações da mensagem
+                message_type, process_id, _ = data.decode().split(Message.SEPARATOR)
+                process_id = int(process_id)
 
-              if process_id not in self.processes:
-                  self.add_process(process_id, addr)
-              if message_type == Message.REQUEST:
-                  self.queue.append(process_id)
-                  log_event(
-                      f"[REQUEST] Mensagem enviada pelo processo {process_id} para solicitar acesso região critica.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-                  if self.isGranted == False:
-                      if process_id in self.vezes_atendidos:
-                          self.vezes_atendidos[process_id] += 1
-                      else:
-                          self.vezes_atendidos[process_id] = 1
+                # Verifica se o processo já está registrado e adiciona caso não esteja
+                if process_id not in self.processes:
+                    self.add_process(process_id, addr)
+                # Se a mensagem for do tipo REQUEST
+                if message_type == Message.REQUEST:
+                    # Adiciona o processo na fila
+                    self.queue.append(process_id)
+                    # Registra no log que o processo solicitou acesso à região crítica
+                    log_event(
+                        f"[REQUEST] Mensagem enviada pelo processo {process_id} para solicitar acesso região critica.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+                    # Se o acesso não foi concedido a nenhum processo
+                    if self.isGranted == False:
+                        # Atualiza o contador de vezes que o processo foi atendido
+                        if process_id in self.vezes_atendidos:
+                            self.vezes_atendidos[process_id] += 1
+                        else:
+                            self.vezes_atendidos[process_id] = 1
 
-                          
-                      self.send_message(Message.GRANT, process_id)
-                      log_event(
-                          f"[GRANT] Mensagem enviada pelo coordenador dando acesso região critica. Acesso concedido ao processo:{process_id}.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-                      time.sleep(1)
-                      message = Message.create_message(Message.RELEASE, process_id)
-                      log_event(
-                      f"[RELEASE] Mensagem enviada pelo processo {process_id} ao sair da região critica.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-                      log_event(
-                          f"Formato REQUEST - {message}")
-                      time.sleep(1)
+                        # Concede o acesso à região crítica para o processo
+                        self.send_message(Message.GRANT, process_id)
+                        # Registra no log que o acesso foi concedido
+                        log_event(
+                            f"[GRANT] Mensagem enviada pelo coordenador dando acesso região critica. Acesso concedido ao processo:{process_id}.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+                        time.sleep(1)
+                        # Envia mensagem de RELEASE
+                        message = Message.create_message(
+                            Message.RELEASE, process_id)
+                        log_event(
+                            f"[RELEASE] Mensagem enviada pelo processo {process_id} ao sair da região critica.  {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+                        log_event(f"Formato REQUEST - {message}")
+                        time.sleep(1)
 
-                      self.isGranted = False
-                  else:
-                      print("Aguardando processo sair da região critica...")
+                        self.isGranted = False
+                    else:
+                        print("Aguardando processo sair da região critica...")
             except:
                 print("NENHUM PROCESSO CONECTADO...")
-            
 
+    # Função para enviar mensagens aos processos
     def send_message(self, message_type, process_id):
         message = Message.create_message(message_type, process_id)
         self.socket.sendto(message.encode(), self.processes[process_id])
 
+    # Função para adicionar um processo e seu endereço
     def add_process(self, process_id, addr):
         self.processes[process_id] = addr
         return process_id
 
+    # Função para a interface do usuário
     def interface(self):
         while True:
             print("Bem vindo ao servidor!")
@@ -93,9 +115,8 @@ class Server:
             elif command == "2":
                 self.mutex.acquire()
                 for process_id in self.vezes_atendidos:
-                  print(f"Processo {process_id} foi atendido {self.vezes_atendidos[process_id]} vezes.")
-               
-                    
+                    print(
+                        f"Processo {process_id} foi atendido {self.vezes_atendidos[process_id]} vezes.")
                 self.mutex.release()
             elif command == "3":
                 self.running = False
@@ -107,6 +128,7 @@ class Server:
                 break
 
 
+# Inicializa e inicia o servidor
 if __name__ == "__main__":
     server = Server()
     server.start()
